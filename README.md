1f8dd5db3a2f8976e37405f5eabd877fd276bfb3
Ayakura Yuki <YukiAyakura@outlook.com>
2023-08-23 12:56:55 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # cloudflare-r2-static-pages-worker-rs
+
 A Cloudflare Worker that host static pages with R2, building by Rust.
+
+There are some rules that I customized:
+
+1. Define the `Content-Type` of Apple Universal Links config `apple-app-site-association` to `application/json`.
+2. Define the `index.html` file as the default pages entrance in R2.
+
+## Deploy
+
+[![Deploy to Cloudflare Workers](https://deploy.workers.cloudflare.com/button)](https://deploy.workers.cloudflare.com/?url=https://github.com/AyakuraYuki/cloudflare-r2-static-pages-worker-rs/tree/main)
+
+## Wrangler
+
+Wrangler is used to develop, deploy, and configure your Worker via CLI.
+
+Further documentation for Wrangler can be found [here](https://developers.cloudflare.com/workers/tooling/wrangler).
+
+## Usage
+
+With `wrangler`, you can build, test, and deploy your Worker with the following commands:
+
+```sh
+# run your Worker in an ideal development workflow (with a local server, file watcher & more)
+$ npm run dev-remote
+
+# deploy your Worker globally to the Cloudflare network (update your wrangler.toml file for configuration)
+$ npm run deploy
+```
+
+Read the latest `worker` crate documentation here: https://docs.rs/worker
+
+## WebAssembly
+
+`cloudflare-r2-static-pages-worker-rs` (the Rust SDK for Cloudflare Workers used in this worker) is meant to be executed as compiled WebAssembly, and as such so **must** all the code you write and depend upon. All crates and modules used in Rust-based Workers projects have to compile to the `wasm32-unknown-unknown` triple.
+
+Read more about this on the [`cloudflare-r2-static-pages-worker-rs`](https://github.com/AyakuraYuki/cloudflare-r2-static-pages-worker-rs) project README.
+
+## Issues
+
+If you have any problems with the `worker` crate, please open an issue on the upstream project issue tracker on the [`cloudflare-r2-static-pages-worker-rs` repository](https://github.com/AyakuraYuki/cloudflare-r2-static-pages-worker-rs).
---
+++ src/lib.rs
@@ -0,0 +1,99 @@
+use worker::*;
+
+#[event(fetch)]
+async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
+    let path = req.path();
+    let mut key = &path[1..];
+
+    let bucket = match env.bucket("R2") {
+        Ok(b) => b,
+        Err(e) => {
+            console_error!("missing R2 Binding ({})", e);
+            return Response::error("Internal Error", 500);
+        }
+    };
+
+    // visit homepage entry as default if missing key
+    let entrance_value;
+    if key.eq("") {
+        key = match env.var("ENTRANCE") {
+            Ok(entrance) => {
+                entrance_value = entrance.to_string();
+                entrance_value.as_str()
+            }
+            Err(_) => "index.html"
+        };
+    }
+
+    let object_result = bucket.get(key).execute().await;
+    let object_option = match object_result {
+        Ok(option) => option,
+        Err(e) => {
+            console_error!("missing object with key {} ({})", key, e);
+            return Response::error("Object Not Found", 404);
+        }
+    };
+    let object = match object_option {
+        Some(o) => o,
+        None => {
+            console_error!("error object with key {}", key);
+            return Response::error("Object Not Found", 404);
+        }
+    };
+
+    let headers = build_response_headers(key, object.http_etag().as_str());
+
+    object.write_http_metadata(headers.clone()).expect("error write metadata");
+
+    let body_option = match object.body() {
+        Some(option) => option,
+        None => {
+            console_error!("none object bytes with key {}", key);
+            return Response::error("Object Not Found", 404);
+        }
+    };
+
+    match body_option.bytes().await {
+        Ok(bs) => Ok(Response::from_bytes(bs)?.with_headers(headers)),
+        Err(_) => Response::error("Object Not Found", 404)
+    }
+}
+
+fn build_response_headers(key: &str, etag: &str) -> Headers {
+    let mut headers = Headers::new();
+
+    headers
+        .set("Access-Control-Allow-Origin", "*")
+        .expect("Unable to set Access-Control-Allow-Origin");
+    headers
+        .set("Access-Control-Allow-Methods", "GET,HEAD,POST,OPTIONS")
+        .expect("Unable to set Access-Control-Allow-Methods");
+    headers
+        .set("Access-Control-Allow-Headers", "*")
+        .expect("Unable to set Access-Control-Allow-Headers");
+    headers
+        .set("Access-Control-Max-Age", "86400")
+        .expect("Unable to set Access-Control-Max-Age");
+
+    headers
+        .set("Vary", "Origin")
+        .expect("Unable to set Vary");
+
+    let guess = mime_guess::from_path(key);
+    if let Some(raw_mime) = guess.first_raw() {
+        headers
+            .set("Content-Type", raw_mime)
+            .expect("Unable to set Content-Type");
+    }
+    if key.eq("apple-app-site-association") {
+        headers
+            .set("Content-Type", "application/json")
+            .expect("Unable to set Content-Type");
+    }
+
+    headers
+        .set("etag", etag)
+        .expect("Unable to set etag");
+
+    headers
+}
